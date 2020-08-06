7d55ad551704f2c7d1b502c9fbf343ba29e88cfe
Kyler Mejia <kylermejia1@gmail.com>
2020-08-06 21:20:05 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,8 @@
+new_array = []
+
 def square_array(array)
  array.each do |integer|
-  new_array = [integer ** 2, integer **2, integer **2]
-  return new_array
+  new_array << integer ** 2
  end
+ return new_array
 end
